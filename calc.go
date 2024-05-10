8ed27dd382d6b35bf376e9fe5c38e5c66fb2f4fe
IxxyDev <denisenkoforwork@gmail.com>
2024-05-10 19:11:21 +0300

    feat: add roman to int converter

---
+++ calc.go
@@ -0,0 +1,20 @@
+package main
+
+func romanToInt(romanNumber string) int {
+	result := 0
+	romanToIntMap := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+	}
+
+	for i := 0; i < len(romanNumber); i++ {
+		current := int(romanNumber[rune(romanNumber[i])])
+
+		if i+1 < len(romanNumber) && romanToIntMap[rune(romanNumber[i+1])] > current {
+			result -= current
+		} else {
+			result += current
+		}
+	}
+}
